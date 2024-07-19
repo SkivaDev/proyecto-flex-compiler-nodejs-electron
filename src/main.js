97ac988346dc1175cc3737670d99c96baf8c9b46
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-07-18 20:28:20 -0500

    feat(project): :art: update screen size, and logo utp

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ const os = require('os');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1000,
+        height: 800,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
